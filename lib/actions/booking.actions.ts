0ea63b89a5ffb02771b2ea95f1957d2c69771e6d
praneethb7 <praneethbudati.work@gmail.com>
2026-02-21 00:51:01 +0530

    pushh

--- lib/actions/booking.actions.ts
+++ lib/actions/booking.actions.ts
@@ -5,7 +5,7 @@ import {Booking} from "@/database";
 export const createBooking = async ({eventId, slug, email}:{eventId:string,slug:string,email:string}) => {
     try {
         await connectDB();
-        await Booking.create({eventId, slug, email});
+        const booking = (await Booking.create({eventId, slug, email})).lean();
         return {success: true};
     } catch (e) {
         console.error(e);
--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -3,14 +3,26 @@
 import connectDB from "@/lib/mongodb";
 import Event from "@/database/event.model";
 
-export const getSimilarEventBySlug = async(slug:string) => {
-    try{
-        await  connectDB();
+export const getSimilarEventBySlug = async (slug: string) => {
+    try {
+        await connectDB();
 
-        const event = await Event.findOne({slug});
-        return await Event.find({_id: {$ne: event._id}, tags: {$in:event.tags}}).lean();
+        const event = await Event.findOne({ slug }).lean();
+        if (!event) return [];
+
+        const similarEvents = await Event.find({
+            _id: { $ne: event._id },
+            tags: { $in: event.tags },
+        }).lean();
+
+        return similarEvents.map((e) => ({
+            ...e,
+            _id: e._id.toString(),
+            createdAt: e.createdAt?.toISOString(),
+            updatedAt: e.updatedAt?.toISOString(),
+        }));
 
     } catch {
         return [];
     }
-}
+};
