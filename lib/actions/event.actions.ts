afc81caada3f0811b0b1232fcac01a47de3036b1
praneethb7 <praneethbudati.work@gmail.com>
2026-02-21 00:58:01 +0530

    final fix final

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,28 +1,15 @@
 'use server';
 
+import Event from '@/database/event.model';
 import connectDB from "@/lib/mongodb";
-import Event from "@/database/event.model";
 
-export const getSimilarEventBySlug = async (slug: string) => {
+export const getSimilarEventsBySlug = async (slug: string) => {
     try {
         await connectDB();
+        const event = await Event.findOne({ slug });
 
-        const event = await Event.findOne({ slug }).lean();
-        if (!event) return [];
-
-        const similarEvents = await Event.find({
-            _id: { $ne: event._id },
-            tags: { $in: event.tags },
-        }).lean();
-
-        return similarEvents.map((e) => ({
-            ...e,
-            _id: e._id.toString(),
-            createdAt: e.createdAt?.toISOString(),
-            updatedAt: e.updatedAt?.toISOString(),
-        }));
-
+        return await Event.find({ _id: { $ne: event._id }, tags: { $in: event.tags } }).lean();
     } catch {
         return [];
     }
-};
+}
